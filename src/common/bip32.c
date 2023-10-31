b57bda7ee9824620069b102589bb422ad784f78d
timemarkovqtum <time.markov@qtum.com>
2023-10-31 13:04:03 +0100

    Fix fuzz tests issues

--- src/common/bip32.c
+++ src/common/bip32.c
@@ -36,7 +36,7 @@ bool bip32_path_read(const uint8_t *in, size_t in_len, uint32_t *out, size_t out
     size_t offset = 0;
 
     for (size_t i = 0; i < out_len; i++) {
-        if (offset > in_len) {
+        if (offset + 4 > in_len) {
             return false;
         }
         out[i] = read_u32_be(in, offset);
